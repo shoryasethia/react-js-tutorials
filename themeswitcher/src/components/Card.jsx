2bd3abcc131c8b72e32bce349ce9b4abf290ea55
Shorya Sethia <132898518+shoryasethia@users.noreply.github.com>
2025-07-01 17:09:34 +0530

    Updated card in themeswitcher

--- themeswitcher/src/components/Card.jsx
+++ themeswitcher/src/components/Card.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
-
 export default function Card() {
     return (
         <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="/">
-                <img className="p-8 rounded-t-lg" src="https://images.pexels.com/photos/18264716/pexels-photo-18264716/free-photo-of-man-people-laptop-internet.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="product_image1" />
+                <img className="p-8 rounded-t-lg" src="https://github.com/shoryasethia/markdrop/blob/main/markdrop/src/markdrop-logo.png?raw=true" alt="product_image1" />
             </a>
             <div className="px-5 pb-5">
                 <a href="/">
                     <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                        Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+                        A Python package for converting PDFs to markdown while extracting images and tables, generate descriptive text descriptions for extracted tables/images using several LLM clients. And many more functionalities. Markdrop is available on PyPI.
                     </h5>
                 </a>
                 <div className="flex items-center mt-2.5 mb-5">
@@ -63,12 +61,12 @@ export default function Card() {
                     </span>
                 </div>
                 <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+                    <span className="text-3xl font-bold text-gray-900 dark:text-white">128+ Stars</span>
                     <a
-                        href="/"
+                        href="https://github.com/shoryasethia/markdrop"
                         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     >
-                        Add to cart
+                        Github
                     </a>
                 </div>
             </div>
