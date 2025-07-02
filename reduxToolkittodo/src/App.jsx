d34b5025675bbe4cc75edaeed4457d8978bbe234
Shorya Sethia <132898518+shoryasethia@users.noreply.github.com>
2025-07-02 17:06:41 +0530

    Added todo in redux toolkit

--- reduxToolkittodo/src/App.jsx
+++ reduxToolkittodo/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
-
+  
   return (
     <>
-      <h1 className="">Hello World!</h1>
+      <h1 className='bg-gray-200 text-3xl p-4'>Redux Tool Kit | Todo List</h1>
+      <AddTodo />
+      <Todos />
     </>
   )
 }
